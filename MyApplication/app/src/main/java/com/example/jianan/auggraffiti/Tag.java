2bf70bd0bd55d51b2b09f637927b2a3226c8c952
canhunan1 <jianan205@gmail.com>
2016-10-12 22:22:27 -0700

    Comment ImageActivity and GraphView

--- MyApplication/app/src/main/java/com/example/jianan/auggraffiti/Tag.java
+++ MyApplication/app/src/main/java/com/example/jianan/auggraffiti/Tag.java
@@ -5,6 +5,7 @@ import com.google.android.gms.maps.model.LatLng;
 /**
  * Created by Jianan on 9/17/2016.
  * Tag is used to save the information of tags including the tagId, latitude and longitude
+ * @author Jianan Yang
  */
 public class Tag {
         int tagId;
